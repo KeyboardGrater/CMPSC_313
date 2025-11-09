ebceb963f546fb769eb4ddc5a4418d594b99a758
KeyboardGrater <dweltBadger@proton.me>
2025-11-09 17:43:32 -0500

    Enhance set.asm with print_switch and comments
    Added print_switch for printing sets and updated comments for clarity.

--- set.asm
+++ set.asm
@@ -6,6 +6,7 @@ INTEGERSET_ARRAY_SIZE = 100                 # enum {INTEGERSET_ARRAY_SIZE = 100}
 
 # jump tables
 operation_switch: .word case_1, case_2, case_3, case_4, case_5, case_6, case_7
+print_switch: .word ps_1, ps_2, ps_3
 
 # strings
 operation_prompt: .asciiz "Please pick a operation to perform.\n1: Union of two sets\n2: Intersection of two sets\n3: Insertion on a set\n4: Deletion on a set\n5: Print a set\n6: Check to see if two sets are equal\n7: Exit\n\n"
@@ -21,8 +22,8 @@ testMessage: .asciiz "Test Message"
 
 main:
     # ------------------------ Initalization part ---------------------------- #
-    # t0 = bytes to allocate ---> Done after both structs are created
-    # s1 = address_1, s2 = address_2
+    # t0 = bytes to allocate ---> Done after both structs are created, t3 = temp
+    # s1 = address_1, s2 = address_2, s3 = INTEGERSET_ARRAY_SIZE - 1
 
     # calaculate the number of bytes that need to be allocated.
     li $t0, 4                                # int = 4 bytes
@@ -42,6 +43,10 @@ main:
     syscall
     move $s2, $v0
 
+    # last_index_of_array = INTEGERSET_ARRAY_SIZE - 1
+    li $s3, INTEGERSET_ARRAY_SIZE
+    addi $s3, $s3, -1
+
     # constructor(&array_1);
     move $a0, $s1                           # move to address register which simulates arguments
     jal constructor
@@ -53,7 +58,8 @@ main:
 
 
     # ----------------------------------- User Choice Section --------------------------
-    # t1 = operation_choice , t2 = array_choice, t3 = temp 
+    # t1 = operation_choice , t2 = array_choice, t3 = temp, t4 = array_choice_offset
+    # s1 = array_1, s2 = array_2
 
     user_choices_loop:
         # Get user input for what operation they want to do.
@@ -108,10 +114,41 @@ main:
             j user_choices_loop
         # Print
         case_5:
+            # Case 1, 2, 3, will all make the operations menu appear again. 1 and 2 does a action beforehand, while 3 does no actions.
+            # The default case (when not 1, 2, nor 3) repeats the array_choice, but still within the print action choice.
             operation_choice_print_loop:
                 # array_choice = which_array_to_choose();
+                jal which_array_to_choose
+                move $t2, $v0
+                
+                # switch (array_choice)
+                
+                # Account for zero-indexing
+                addi $t2, $t2, -1
 
-            j user_choices_loop
+                # Find the location of the array choice
+                la $t3, print_switch                  # Base address
+                mul $t4, $t2, 4                       # array_choice * sizeOf(int)
+                add $t3, $t3, $t4                     # t3 = base_addr + offset
+
+                # Goto Case
+                lw $t3, 0($t3)
+                jr $t3
+
+                ps_1:
+                    # print_set(&array_1);
+                    move $a0, $s1                     # load into argument array_1_addr 
+                    move $a1, $s3                     # load last_index into arguments
+                    jal print_set                     
+                    j user_choices_loop               # Repeat the users operation options
+                ps_2:
+                    # print_set(&array_2);
+                    move $a0, $s2
+                    move $a1, $s3
+                    jal print_set
+                    j user_choices_loop
+                ps_3:
+                    j user_choices_loop               
         # Equals
         case_6:
             # TODO
@@ -166,13 +203,13 @@ which_array_to_choose:
     li $t2, 1
     li $t3, 3
 
-    # printf("Which array would you like to chose.\nFor array one enter 1, for array two, enter 2. And if you would like to go back, enter 3.\n"); 
-    li $v0, 4
-    la $a0, which_array_prompt
-    syscall
-
     # Get user input for which array they would like to modify.
-    which_array_to_choose_loop
+    which_array_to_choose_loop:
+        # printf("Which array would you like to chose.\nFor array one enter 1, for array two, enter 2. And if you would like to go back, enter 3.\n"); 
+        li $v0, 4
+        la $a0, which_array_prompt
+        syscall
+
         li $v0, 5
         syscall
         move $t1, $v0
@@ -183,6 +220,44 @@ which_array_to_choose:
 
         # When in the bounds
         move $v0, $t1
+
+    jr $ra
+
+print_set:
+    # Arguments: a0 = struct Integer Set * set_pointer, a1 = unsigned int last_index
+    # t0 = i, t1 = last_index, t2 = offset, t3 = curr_addr, t4 = base_addr
+    # a0 = curr_val
+
+    li $t0, 0                               # int i = 0;
+    move $t4, $a0
+    move $t1, $a1                           
+
+    # Loop over set and print at index
+    print_set_loop:
+        # Conditional to check if we should exit
+        bgt $t0, $t1, print_set_loop_exit
+        
+        # unsigned int value_at_index = set_pointer -> a[i];
+        # Get value at i
+        mul $t2, $t0, 4                     # offset = t2 = i * 4
+        add $t3 , $t4, $t2                  # curr_addr = base_addr + offset
+        lw $a0, 0($t3)                      # load value at address t3 into a0
+
+        #printf("%u", value_at_index);                          
+        # Print value
+        li $v0, 1
+        syscall
+        
+        # Print spaces in between numbers
+        li $v0, 11
+        la $a0, 20                          
+        syscall
+
+        # increment the iterator
+        addi $t0, $t0, 1
+
+        j print_set_loop
+    print_set_loop_exit:
 
     jr $ra
 
