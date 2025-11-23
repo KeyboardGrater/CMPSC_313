e54c38c1adc8d52e1ea90bfb81d19a1763e397e8
KeyboardGrater <DweltBadger@proton.me>
2025-11-22 21:32:25 -0500

    Added a balance file read function

--- homework/5/mips/savingsAccountFile.asm
+++ homework/5/mips/savingsAccountFile.asm
@@ -8,69 +8,88 @@
 #     double savings_balance;               # 8 bytes
 # } SavingsAccount;                         # 4 + 8 + 8 = 20 bytes
 
+# Constrants
+READ_FILE_AMMOUNT = 256
+
+
 # Array
+.align 2
 decimal_array: .space 8                     # int * 2 = 8
 
-# Test Data   
+# Buffers
+read_buffer: .space READ_FILE_AMMOUNT 
+sub_buffer: .space READ_FILE_AMMOUNT
+temp_buffer: .space READ_FILE_AMMOUNT
 
-# Strings
-new_balance_message: .asciiz "New Balance: "
+str_A: .space READ_FILE_AMMOUNT
+str_B: .space READ_FILE_AMMOUNT
+
+# Switch statements
+balance_loop_switch: .word bls_0 bls_1
+
+# Paths
+balance_file_path: .asciiz "/home/logan/Documents/classes/313_compsci/homework/5/mips/balance.txt"
+transaction_file_path: .asciiz "/home/logan/Documents/classes/313_compsci/homework/5/mips/copy_transaction.txt"
+
+# Constants continued
+hundred_placed: .double 100.00
+
+# Messages
+open_balance_error_message: .asciiz "There was a issue when trying to open the balance file"
+
+num_created_accounts_message: .asciiz "Accounts Created: "
+
+test_message: .asciiz "THIS POINT HAS BEEN REACHED"
+
 
 .text
 .globl main
 main:
-    # ---------------------- Initialization Part -----------------
+
+    # --------------- 1. Setting up the accounts --------------- #
+
+    # Open the balance file
+    li $v0, 13                              # file_pointer = fopen(balance_file_path, "r");
+    la $a0, balance_file_path
+    la $a1, 0
+    la $a2, 0
+    syscall
+    move $s0, $v0                           # Save the file descripter
+
+    # Check to see if the file opened correctly
+    blt $s0, $zero, open_balance_error
     
-    # First Open the file
-    
-
-
-
-
-    
-    
-# ------------------------------- Intalization End -------------------------------
-    # s0 = account_1_addr, s1 = account_2_addr
-    # f0 = new annual interest rate
-    
-    # Calculate the monthly interest
-
-    # account 1
-    move $a0, $s0
-    jal calculate_monthly_interest
-
-    # account 2
-    move $a0, $s1
-    jal calculate_monthly_interest
-
-    # change annual interest rate
-    l.d $f0, account_interest_update
-
-    # save f0 to stack
+    # Create the accounts based off the balance file
+    # SavingsAccount ** account = read_balance_file(file_pointer, &num_accounts, READ_FILE_AMMOUNT);
     addi $sp, $sp, -4
-    s.d $f0, 0($sp)
-
+    sw $ra, 0($sp)
+    
     move $a0, $s0
-    mov.d $f12, $f0 
-    jal set_interest_rate
-
-    # pop stack for f0
-    l.d $f0, 0($sp)
-    addi $sp, $sp, 4
-
-    move $a0, $s1
-    mov.d $f12, $f0
-    jal set_interest_rate
-
-    # Print the new balances
+    jal read_balance_file
+    move $s1, $v0                           # s1 = account array pointer
+    move $s2, $v1                           # s2 = num_accounts
+    
+    # Close the file 
+    li $v0, 16
     move $a0, $s0
-    jal calculate_monthly_interest
+    syscall
+    
+    # List the number of created classes
+    li $v0, 4
+    la $a0, num_created_accounts_message
+    syscall
 
-    move $a0, $s1
-    jal calculate_monthly_interest
+    li $v0, 1
+    move $a0, $s2
+    syscall
+
+    li $v0, 11
+    la $a0, 0x0A
+    syscall
+
+
 
     j end_program
-
 
 new_account:
     # SavingsAccount * new_account (SavingsAccount * account_X, unsigned int account_number, double annaul_interest_rate, double savings_balance) 
@@ -88,6 +107,8 @@ new_account:
     sw $t1, 0($t0)                          # save the account_number to the first segment of the struct
     s.d $f0, 4($t0)                         # save the yearly_interest_rate to the second segment of the struct (at base_addr + int = 0 + 4 = 4)
     s.d $f2, 12($t0)                        # save the balance to the third segment of the structure (at base_addr + int + double = 0 + 4 + 8 = 12)
+    
+    move $v0, $t0
 
     jr $ra
 
@@ -279,7 +300,396 @@ set_interest_rate:
     s.d $f0, 4($t0)
 
     jr $ra
+
+read_balance_file:
+    # Arguments: a0 = file_description
+    # t0 = i, t1 = bytes_read, t2 = current_charcter, t3 = count, t4 = j, t5 = id_or_balance, t6 = account_id
+    # t8 = temp1, t9 = temp2
+    # a1 = read_buffer, a2 = sub_buffer, a3 = temp_buffer
+    # s1 = SavingsAccount ** account
     
+    # Save registers we'll need to preserve
+    addi $sp, $sp, -12
+    sw $ra, 0($sp)
+    sw $s1, 4($sp)
+    sw $s2, 8($sp)                          # s2 will store account count
+
+    # Move the file description to t8 temporarily
+    move $t8, $a0
+
+    # Read the file and then store it into a buffer
+    li $v0, 14
+    move $a0, $t8
+    la $a1, read_buffer
+    li $a2, READ_FILE_AMMOUNT
+    syscall
+    move $t1, $v0                           # t1 = bytes_read
+    
+    # Initialize variables
+    li $t0, 0                               # i = 0
+    li $t3, 0                               # count = 0
+
+    balance_memory_loop:
+        # Exit conditional (i == bytes_read)
+        beq $t0, $t1, balance_memory_loop_exit
+
+        # Check if the current_charcter is a newline
+        la $a1, read_buffer
+        add $t2, $a1, $t0                   # curr_addr = base_addr + offset
+        lb $t2, 0($t2)
+
+        li $t9, 0x0A
+        bne $t2, $t9, increment_count_branch_skip
+        addi $t3, $t3, 1                    # count++
+        
+    increment_count_branch_skip:
+        addi $t0, $t0, 1                    # i++
+        j balance_memory_loop
+        
+    balance_memory_loop_exit:
+
+    # Check to see if the last charcter in the buffer is not a newline
+    li $t9, 0x0A
+    beq $t2, $t9, balance_cc_ne_nl_skip
+    
+    # Add newline if last char isn't one
+    la $a1, read_buffer
+    add $t8, $a1, $t1                       # curr_addr = base_addr + bytes_read
+    li $t9, 0x0A
+    sb $t9, 0($t8)                          # read_buffer[bytes_read] = 0x0A
+    addi $t1, $t1, 1                        # bytes_read++
+    addi $t3, $t3, 1                        # count++
+    
+balance_cc_ne_nl_skip:
+
+    # Allocate array of pointers: account = malloc(count * 4)
+    sll $a0, $t3, 2                         # count * 4 (size of pointer)
+    li $v0, 9
+    syscall
+    move $s1, $v0                           # s1 = account array pointer
+    move $s2, $t3                           # s2 = total count (save for later)
+
+    # Initialize parsing variables
+    li $t0, 0                               # i = 0
+    li $t3, 0                               # count = 0 (reuse as array index)
+    li $t4, 0                               # j = 0
+    li $t5, 0                               # id_or_balance = 0
+    la $a1, read_buffer
+    la $a2, sub_buffer
+    la $a3, temp_buffer
+
+    # Loop over the buffer and parse the data
+    read_balance_file_loop:
+        # Exit condition (i == bytes_read)
+        beq $t0, $t1, read_balance_file_loop_exit
+
+        # Get current character: current_charcter = read_buffer[i]
+        add $t8, $a1, $t0
+        lb $t2, 0($t8)
+
+        # Check if character is newline
+        li $t9, 0x0A
+        bne $t2, $t9, balance_charcter_is_newline_skip
+        
+        # Character is a newline
+        # sub_buffer[j] = 0x00
+        add $t8, $a2, $t4
+        sb $zero, 0($t8)
+        li $t4, 0                           # j = 0 (reset)
+
+        # switch(id_or_balance)
+        beqz $t5, bls_0                     # if id_or_balance == 0
+        # else it's case 1
+        j bls_1
+
+        bls_0:
+            # case 0: strcpy(temp_buffer, sub_buffer)
+            # Save registers that might be overwritten
+            addi $sp, $sp, -24
+            sw $t0, 0($sp)
+            sw $t1, 4($sp)
+            sw $t2, 8($sp)
+            sw $t3, 12($sp)
+            sw $t4, 16($sp)
+            sw $t5, 20($sp)
+            
+            move $a0, $a3                   # dest = temp_buffer
+            move $a1, $a2                   # src = sub_buffer
+            jal strcpy
+            
+            # Restore registers
+            lw $t0, 0($sp)
+            lw $t1, 4($sp)
+            lw $t2, 8($sp)
+            lw $t3, 12($sp)
+            lw $t4, 16($sp)
+            lw $t5, 20($sp)
+            addi $sp, $sp, 24
+            
+            # Reload buffer addresses
+            la $a1, read_buffer
+            la $a2, sub_buffer
+            la $a3, temp_buffer
+            
+            li $t5, 1                       # id_or_balance = 1
+            j balance_iterator_update
+
+        bls_1:
+            # case 1: Parse account_id and balance, create account
+            # Save all registers before function calls
+            addi $sp, $sp, -32
+            sw $t0, 0($sp)
+            sw $t1, 4($sp)
+            sw $t2, 8($sp)
+            sw $t3, 12($sp)
+            sw $t4, 16($sp)
+            sw $t5, 20($sp)
+            sw $a1, 24($sp)
+            sw $a2, 28($sp)
+            
+            # account_id = atoi(temp_buffer)
+            move $a0, $a3
+            jal atoi
+            move $t6, $v0                   # t6 = account_id
+            
+            # Restore a2 for strtod call
+            lw $a2, 28($sp)
+            
+            # balance = strtod(sub_buffer, &double_ptr)
+            move $a0, $a2
+            jal strtod
+            mov.d $f2, $f0                  # f2 = balance
+            
+            # Allocate memory for new SavingsAccount (20 bytes)
+            li $v0, 9
+            li $a0, 20
+            syscall
+            move $t7, $v0                   # t7 = new account pointer
+            
+            # Store account pointer in array: account[count] = new_account_ptr
+            lw $t3, 12($sp)                 # restore count
+            sll $t8, $t3, 2                 # count * 4
+            add $t8, $s1, $t8               # account + (count * 4)
+            sw $t7, 0($t8)                  # account[count] = new_account_ptr
+            
+            # Call new_account(account_ptr, account_id, -1.0, balance)
+            move $a0, $t7                   # account pointer
+            move $a1, $t6                   # account_id
+            li $t8, -1
+            mtc1 $t8, $f12
+            cvt.d.w $f12, $f12              # f12 = -1.0
+            mov.d $f14, $f2                 # f14 = balance
+            jal new_account
+            
+            # Restore all registers
+            lw $t0, 0($sp)
+            lw $t1, 4($sp)
+            lw $t2, 8($sp)
+            lw $t3, 12($sp)
+            lw $t4, 16($sp)
+            lw $t5, 20($sp)
+            lw $a1, 24($sp)
+            lw $a2, 28($sp)
+            addi $sp, $sp, 32
+            
+            # Reload buffer addresses
+            la $a1, read_buffer
+            la $a2, sub_buffer
+            la $a3, temp_buffer
+            
+            addi $t3, $t3, 1                # count++
+            li $t5, 0                       # id_or_balance = 0
+            j balance_iterator_update
+
+    balance_charcter_is_newline_skip:
+        # Character is not newline
+        # sub_buffer[j] = current_charcter
+        add $t9, $a2, $t4
+        sb $t2, 0($t9)
+        addi $t4, $t4, 1                    # j++
+
+    balance_iterator_update:
+        addi $t0, $t0, 1                    # i++
+        j read_balance_file_loop
+        
+    read_balance_file_loop_exit:
+
+    # Return values
+    move $v0, $s1                           # v0 = account array pointer
+    div $s2, $s2, 2
+    move $v1, $s2                           # v1 = num_accounts
+    
+    # Restore saved registers
+    lw $ra, 0($sp)
+    lw $s1, 4($sp)
+    lw $s2, 8($sp)
+    addi $sp, $sp, 12
+    
+    jr $ra
+
+strtod:
+    # Arguments: a0 = string (format: "XXX.YY" - always 2 decimal places)
+    # Returns: f0 = double value
+    # t0 = i, t1 = current_char, t2 = temp addr, t3 = counter for strB
+    
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+    
+    li $t0, 0                               # i = 0
+    la $a1, str_A
+    la $a2, str_B
+    
+    strtod_find_decimal_loop:
+        # Load the current_character
+        add $t1, $a0, $t0                       # curr_addr = base_addr + i
+        lb $t1, 0($t1)                          # load character
+        
+        # Check if we hit decimal point
+        beq $t1, 0x2E, strtod_find_decimal_loop_exit  # '.' = 0x2E
+        beqz $t1, strtod_find_decimal_loop_exit # null terminator (shouldn't happen)
+        
+        # Save character into strA
+        add $t2, $a1, $t0
+        sb $t1, 0($t2)
+        
+        addi $t0, $t0, 1                        # i++
+        j strtod_find_decimal_loop
+        strtod_find_decimal_loop_exit:
+   
+    # Add null terminator to strA
+    add $t2, $a1, $t0
+    sb $zero, 0($t2)
+    
+    addi $t0, $t0, 1                        # Skip the decimal point
+    li $t3, 0                               # counter for strB
+    
+    strtod_get_second_loop:
+        # Load the current character
+        add $t1, $a0, $t0
+        lb $t1, 0($t1)
+        
+        # Check for null terminator, space, or newline
+        beqz $t1, strtod_get_second_loop_exit
+        beq $t1, 0x20, strtod_get_second_loop_exit  # space
+        beq $t1, 0x0A, strtod_get_second_loop_exit  # newline
+        
+        # Save into strB
+        add $t2, $a2, $t3
+        sb $t1, 0($t2)
+        
+        # Increment counters
+        addi $t0, $t0, 1
+        addi $t3, $t3, 1
+        j strtod_get_second_loop
+        strtod_get_second_loop_exit:
+        
+    # Add null terminator to strB
+    add $t2, $a2, $t3
+    sb $zero, 0($t2)
+    
+    # Call atoi for strA (integer part)
+    move $a0, $a1
+    jal atoi
+    move $t0, $v0                           # t0 = integer part
+    
+    addi $sp, $sp, -4
+    sw $t0, 0($sp)
+
+    # Call atoi for strB (decimal part)
+    move $a0, $a2
+    jal atoi
+    move $t1, $v0                           # t1 = decimal part
+    
+    lw $t0, 0($sp)
+    addi $sp, $sp, 4
+
+    # Convert integer part to double
+    mtc1 $t0, $f0
+    cvt.d.w $f0, $f0
+    
+    # Convert decimal part to double
+    mtc1 $t1, $f2
+    cvt.d.w $f2, $f2
+    
+    # Divide by 100 (since always 2 decimal places)
+    li $t5, 100
+    mtc1 $t5, $f4
+    cvt.d.w $f4, $f4
+    div.d $f2, $f2, $f4
+    
+    # Add integer and decimal parts
+    add.d $f12, $f0, $f2
+    
+    li $v0, 3
+    syscall
+    
+    li $v0, 11
+    la $a0, 0x0A
+    syscall
+
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
+    
+
+
+strcpy:
+    # Arguments: a0 = string to copy into, a1 = string to copy from
+    
+    
+    move $v0, $a0                           # save original dest for return
+    
+strcpy_loop:
+    lb $t8, 0($a1)                          # load byte from src
+    sb $t8, 0($a0)                          # store byte to dest
+    # beqz $t8, strcpy_return                 # if null terminator, done
+    beq $t8, 0x00, strcpy_return
+    addi $a0, $a0, 1                        # advance dest
+    addi $a1, $a1, 1                        # advance src
+    j strcpy_loop
+
+strcpy_return:
+    jr $ra
+
+
+atoi:
+    or		$v0 , $0, $0		# $v0  = $0 | $0
+    or		$t1, $0, $0		# $t1 = $0 | $0
+    lb		$t0, 0($a0)		# 
+    bne		$t0, '+', .isp	# if $t0 != '+' then goto .isp
+    addi	$a0, $a0, 1			# $a0 = $a0 + 1
+.isp:
+    lb		$t0, 0($a0)		# 
+    bne		$t0, '-', .num	# if $t0 != '-' then goto .num
+    addi	$t1, $0, 1			# $t1 = $0 + 1
+    addi	$a0, $a0, 1			# $a0 = $a0 + 1
+.num:
+    lb		$t0, 0($a0)		# 
+    slti	$t2, $t0, 58			# $t2 = ($t0 < 58) ? 1 : 0
+    slti	$t3, $t0, '0'			# $t3 = ($t0 <'0'0) ? 1 : 0
+    beq		$t2, $0, .done	# if $t2 == $0 then goto .done
+    bne		$t3, $0, .done	# if $t3 != $0 then goto .done
+    sll		$t2, $v0, 1			# $t2 = $v0 << 1
+    sll		$v0, $v0, 3			# $v0 = $v0 << 3
+    add		$v0, $v0, $t2		# $v0 = $v0 + $t2
+    addi	$t0, $t0, -48			# $t0 = $t0 + -48
+    add		$v0, $v0, $t0		# $v0 = $v0 + $t0
+    addi	$a0, $a0, 1			# $a0 = $a0 + 1  num++
+    j .num
+
+.done:
+    beq		$t1, $0, .out	# if $t1 == $0 then goto .out
+    sub		$v0, $0, $v0		# $v0 = $t0- $t2
+.out:
+    jr $ra
+
+open_balance_error:
+    li $v0, 4
+    la $a0, open_balance_error_message
+    syscall
+
+    j end_program
+
 
 # End program
 end_program:
